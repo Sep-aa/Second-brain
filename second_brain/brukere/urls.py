65ec3a42992e9392f0d8c87dadebcac2c46ebcd6
oliver12345678goodboy <oliverasnes@gmail.com>
2026-01-28 14:44:34 +0100

    Add main page and template inheritance
    Introduces a main view and template, refactors existing templates to use master.html for inheritance, and updates URL patterns to include the main page. This improves maintainability and provides a consistent layout across pages.

--- second_brain/brukere/urls.py
+++ second_brain/brukere/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.main, name='main'),
     path('brukere/', views.brukere, name='brukere'),
     path('brukere/detaljer/<int:id>', views.detaljer, name='detaljer'),
 ]
