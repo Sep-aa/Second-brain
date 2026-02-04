77d3336b0075e8fba89ecc321c02182b5cfb08c8
Sebastian <seb18061@skole.telemarkfylke.no>
2026-02-04 13:07:13 +0100

    Refactor homepage app: update URLs, add views, and create templates

--- second_brain/brukere/urls.py
+++ second_brain/brukere/urls.py
@@ -1,8 +1,13 @@
+# -*- coding: utf-8 -*-
+# @Author: Sep-aa
+# @Date:   2026-02-04 12:37:16
+# @Last Modified by:   Sep-aa
+# @Last Modified time: 2026-02-04 13:00:33
 from django.urls import path
 from . import views
 
+app_name = 'brukere'
 urlpatterns = [
-    path('', views.main, name='main'),
-    path('brukere/', views.brukere, name='brukere'),
-    path('brukere/detaljer/<int:id>', views.detaljer, name='detaljer'),
+    path('', views.brukere, name='brukere'),
+    path('<int:id>', views.detaljer, name='detaljer'),
 ]
--- second_brain/homepage/views.py
+++ second_brain/homepage/views.py
@@ -1,3 +1,16 @@
+# -*- coding: utf-8 -*-
+# @Author: Sep-aa
+# @Date:   2026-02-04 12:13:12
+# @Last Modified by:   Sep-aa
+# @Last Modified time: 2026-02-04 12:53:02
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import redirect
 
-# Create your views here.
+def main(request):
+    template = loader.get_template('main.html')
+    return HttpResponse(template.render())
+    
+def home(request):
+    return redirect('brukere')
