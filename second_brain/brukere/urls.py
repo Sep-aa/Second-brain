3af5590a3331a9cf81fa213c1ab97083bc19e745
Seb <seb18061@skole.telemarkfylke.no>
2026-02-04 13:48:01 +0100

    removed unneccesary code

--- second_brain/brukere/urls.py
+++ second_brain/brukere/urls.py
@@ -2,11 +2,10 @@
 # @Author: Sep-aa
 # @Date:   2026-02-04 12:37:16
 # @Last Modified by:   Sep-aa
-# @Last Modified time: 2026-02-04 13:00:33
+# @Last Modified time: 2026-02-04 13:46:45
 from django.urls import path
 from . import views
 
-app_name = 'brukere'
 urlpatterns = [
     path('', views.brukere, name='brukere'),
     path('<int:id>', views.detaljer, name='detaljer'),
--- second_brain/homepage/views.py
+++ second_brain/homepage/views.py
@@ -2,15 +2,10 @@
 # @Author: Sep-aa
 # @Date:   2026-02-04 12:13:12
 # @Last Modified by:   Sep-aa
-# @Last Modified time: 2026-02-04 12:53:02
-from django.shortcuts import render
+# @Last Modified time: 2026-02-04 13:40:52
 from django.http import HttpResponse
 from django.template import loader
-from django.shortcuts import redirect
 
 def main(request):
     template = loader.get_template('main.html')
     return HttpResponse(template.render())
-    
-def home(request):
-    return redirect('brukere')
